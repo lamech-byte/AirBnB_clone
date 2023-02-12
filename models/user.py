2fa202a58fc69903dfb6ce4167f4b7dacbe137f8
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-02-12 21:19:10 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,11 @@
-i
+#!/usr/bin/python3
+"""Module for User class."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Class representing a User."""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
