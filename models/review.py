c83693cfb58456ef122efcf9ab62e5570d4df1e0
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-02-12 21:17:50 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,1 +1,11 @@
-i
+#!/usr/bin/python3
+"""Module for Review class."""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Class representing a Review."""
+    place_id = ""
+    user_id = ""
+    text = ""
