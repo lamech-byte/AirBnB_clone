3ac9cb2b7c7888eb6daa394b3a79a2c358af7c2d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-02-12 21:14:20 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,9 @@
-i
+#!/usr/bin/python3
+"""Module for Amenity class."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class representing a Amenity."""
+    name = ""
