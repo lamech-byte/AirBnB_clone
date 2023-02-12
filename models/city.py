8eee624be3011372ec847b90b38864780ec750ad
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-02-12 21:16:12 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,1 +1,10 @@
-i
+#!/usr/bin/python3
+"""Module for City class."""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class representing a City."""
+    state_id = ""
+    name = ""
